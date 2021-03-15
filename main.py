4fc74098b2ef64699a0e5114efd4ecee65c51efe
DuDSTOPIA <dudscpa@hotmail.com>
2021-03-15 17:49:09 -0300

    creating subtraction, multiplication and division operation

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 
 def calculator():
+    type_math = input('''choose  the type of mathematical calculation you want
+    + for Addition
+    - for Subtraction
+    * for Multiplication
+    / for Division: ''')
+
     x = int(input("Numero x: "))
     y = int(input("numero y: "))
 
-    def soma():
+    if type_math == '+':
         z = x + y
         print("O resultado de {} + {} = {}".format(x, y, z))
-    soma()
+    elif type_math == '-':
+        z = x - y
+        print("O resultado de {} - {} = {}".format(x, y, z))
+    elif type_math == '*':
+        z = x * y
+        print("O resultado de {} * {} = {}".format(x, y, z))
+    elif type_math == '/':
+        z = x / y
+        print("O resultado de {} / {} = {}".format(x, y, z))
+
 
 calculator()
