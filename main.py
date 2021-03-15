702a22161bbdf2df2b1932ea53f1f656670bca08
DuDSTOPIA <dudscpa@hotmail.com>
2021-03-15 18:39:13 -0300

    adding repetition and turning operations into functions

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 def calculator():
     type_math = input('''choose  the type of mathematical calculation you want
     + for Addition
@@ -6,21 +5,57 @@ def calculator():
     * for Multiplication
     / for Division: ''')
 
+    operation(type_math)
+
+
+def operation(type_math):
     x = int(input("Numero x: "))
     y = int(input("numero y: "))
 
     if type_math == '+':
-        z = x + y
-        print("O resultado de {} + {} = {}".format(x, y, z))
+        addition(x, y)
+        again()
+
     elif type_math == '-':
-        z = x - y
-        print("O resultado de {} - {} = {}".format(x, y, z))
+        subtraction(x, y)
+        again()
+
     elif type_math == '*':
-        z = x * y
-        print("O resultado de {} * {} = {}".format(x, y, z))
+        multiplication(x, y)
+        again()
+
     elif type_math == '/':
-        z = x / y
-        print("O resultado de {} / {} = {}".format(x, y, z))
+        division(x, y)
+        again()
+
+    else:
+        print('we do not recognize this symbol, please try again')
 
 
+def addition(x, y):
+    z = x + y
+    print("O resultado de {} + {} = {}".format(x, y, z))
+
+def subtraction(x, y):
+    z = x - y
+    print("O resultado de {} - {} = {}".format(x, y, z))
+
+def multiplication(x, y):
+    z = x * y
+    print("O resultado de {} * {} = {}".format(x, y, z))
+
+def division(x, y):
+    z = x / y
+    print("O resultado de {} / {} = {}".format(x, y, z))
+
+def again():
+    calc_again = input('Do you want to try again? Y for yes and N for no:  ')
+    if calc_again.upper() == 'Y':
+        calculator()
+    elif calc_again == 'N':
+        pass
+    else:
+        print('Sorry, the options are only Y or N')
+        return
 calculator()
+again()
